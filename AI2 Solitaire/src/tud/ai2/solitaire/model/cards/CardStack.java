99e228ee304042ba8659c0aac34bd68996809920
maximilianmitschke <95491679+maximilianmitschke@users.noreply.github.com>
2022-01-02 18:24:37 +0100

    Update CardStack.java

--- AI2 Solitaire/src/tud/ai2/solitaire/model/cards/CardStack.java
+++ AI2 Solitaire/src/tud/ai2/solitaire/model/cards/CardStack.java
@@ -1,15 +1,22 @@
 package tud.ai2.solitaire.model.cards;
 //import java.util.A
 
+/**
+ * 
+ * @author Maximilian Mitschke 
+ * 
+ *
+ */
 public class CardStack extends AbstractCardStack {
 
-    private static int MAX_STACK = 13;
-    private AbstractCard[] a = new AbstractCard [MAX_STACK];
+    private static int MAX_STACK = 13; //maximum size of a stack 
+    private AbstractCard[] a = new AbstractCard [MAX_STACK]; //new Array with the type AbstractCard, with a size of 13 
     private int N = 0;
     
    
-
-
+/**
+ * {@inheritDoc}
+ */
     public void push(AbstractCard cc) {
     	
     	if(a.length<13) {
@@ -18,20 +25,28 @@ public class CardStack extends AbstractCardStack {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public AbstractCard pop() {
-    	if(a.length!=0) {
+    	if(!this.isEmpty()) { //oder direkt auf 0 testen?
     		AbstractCard c = a[a.length-1];
-    		this.remove(a.length-1);
+    		a[a.length-1] = null;
+    		return c;
     	}
-    	return c;
+    	else {
+    		return null;
+    	}
     
     }
 
     public AbstractCard peek() {
-    	if(a.length>0) {
+    	if(a.length>0) { //""
     		return a[a.length-1];
     	}
-    	return null;
+    	else{
+    		return null;
+    	}
     }  
     
     public int size() {
