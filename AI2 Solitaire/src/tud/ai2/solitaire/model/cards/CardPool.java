8a3e599280e1dae896daa5cc6e582fc439f94c17
adrianBohnstedt <93336323+adrianBohnstedt@users.noreply.github.com>
2022-01-02 23:42:56 +0100

    Update CardPool.java

--- AI2 Solitaire/src/tud/ai2/solitaire/model/cards/CardPool.java
+++ AI2 Solitaire/src/tud/ai2/solitaire/model/cards/CardPool.java
@@ -2,7 +2,12 @@ package tud.ai2.solitaire.model.cards;
 
 import java.awt.Container;
 import java.util.Collection;
-
+/**
+ * Implements the card pool with a draw stack and a waste stack
+ * 
+ * @author Adrian Bohnstedt
+ * @version 1.0
+ */
 public class CardPool extends AbstractCardPool {
 
     /**
@@ -19,6 +24,36 @@ public class CardPool extends AbstractCardPool {
         }
         prepare();
     }
+    /**
+     * Gets activated when draw stack is clicked. If draw stack is not empty, the card gets moved to the waste stack
+     */
+	@Override
+	public AbstractCard drawStackClicked() {
+		if(!getDrawStack().isEmpty())
+			getWasteStack().push(getDrawStack().pop());
+		return null;
+	}
+	/**
+	 * Moves all cards from the waste Stack back to the draw stack in the correct order
+	 */
+	@Override
+	public void redeal() {
+		while(!getWasteStack().isEmpty()) {
+			getDrawStack().push(getWasteStack().pop());
+		}
+	}
+	/**
+	 * Removes the first card on the waste stack, if the first card on the waste Stack equals the card parameter and the card parameter is not null.
+	 * 
+	 * @param card the comparison card
+	 */
+	@Override
+	public void removeFromWaste(AbstractCard card) {
+		if(card != null && card.equals(getWasteStack().peek()))
+			getWasteStack().pop();
+		
+	}
 
 
 }
+
