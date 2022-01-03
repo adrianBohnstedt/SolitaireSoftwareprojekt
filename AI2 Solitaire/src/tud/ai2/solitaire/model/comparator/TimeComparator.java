f5607e2ba5a3852b6bee82dff4334fd71d63298a
adrianBohnstedt <93336323+adrianBohnstedt@users.noreply.github.com>
2022-01-03 15:57:08 +0100

    Update TimeComparator.java

--- AI2 Solitaire/src/tud/ai2/solitaire/model/comparator/TimeComparator.java
+++ AI2 Solitaire/src/tud/ai2/solitaire/model/comparator/TimeComparator.java
@@ -1,5 +1,42 @@
 package tud.ai2.solitaire.model.comparator;
 
-public class TimeComparator {
+import java.util.Comparator;
+
+import tud.ai2.solitaire.model.HighscoreEntry;
+/**
+ * Sorts highscore entries by time
+ * 
+ * @author Adrian Bohnstedt
+ * @version 1.0
+ */
+public class TimeComparator implements Comparator<HighscoreEntry>{
+	/**
+	 * Sorts the shorter time above the longer time
+	 * if they are the same sorts usernames
+	 * 
+	 * @param o1 first highscore entry
+	 * @param o2 second highscore entry
+	 */
+	@Override
+	public int compare(HighscoreEntry o1, HighscoreEntry o2) {
+		//sort time by null
+		if(o1.getTime() == null)
+			return 1;
+		if(o2.getTime() == null)
+			return -1;
+		//sort by time
+		if(o1.getTime() < o2.getTime())
+			return 1;
+		if(o1.getTime() > o2.getTime())
+			return -1;
+		//sort name by null
+		if(o1.getName() == null)
+			return 1;
+		else if(o2.getName() == null)
+			return -1;
+		//sort by name
+		return o1.getName().compareToIgnoreCase(o2.getName());
+	}
 
 }
+
