24fabe72bee9bad3481980de177676a9960ea04f
adrianBohnstedt <93336323+adrianBohnstedt@users.noreply.github.com>
2022-01-05 23:30:39 +0100

    Update ResetListener.java

--- AI2 Solitaire/src/tud/ai2/solitaire/util/ResetListener.java
+++ AI2 Solitaire/src/tud/ai2/solitaire/util/ResetListener.java
@@ -1,5 +1,25 @@
 package tud.ai2.solitaire.util;
 
-public class ResetListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import tud.ai2.solitaire.Launch;
+import tud.ai2.solitaire.view.PlayingField;
+/**
+ * Resets the game if needed
+ * 
+ * @author Adrian Bohnstedt
+ */
+public class ResetListener implements ActionListener{
+	/**
+	 * Resets the game if action event is performed
+	 * 
+	 * @param e ActionEvent to reset the game
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO resets the game
+		Launch.activeField = new PlayingField("Solitaire");
+	}
 
 }
